fe63a9d89c2ecfae958c8c154431b06df7a7ed37
tangxing <tangxing@gmail.com>
2019-11-18 17:52:18 +0800

    feat: 软件设计模式案例初始化

---
+++ DesignPattern/src/main/java/com/tang/desgin/Application.java
@@ -0,0 +1,19 @@
+package com.tang.desgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * <p>2019年11月18日</p>
+ * @author tangxing
+ * <p> 软件设计模式的启动类 </p>
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
---
+++ DesignPattern/src/test/java/com/tang/design/test/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.tang.design.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 
+ * <p>2019年11月18日</p>
+ * @author tangxing
+ * <p> SpringbootTest </p>
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationTest {
+
+}
