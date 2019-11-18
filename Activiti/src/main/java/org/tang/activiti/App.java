fae2ec56732b50734bc0820c4678a1197abaf14f
tangxing <17611260510@163.com>
2019-11-18 23:50:02 +0800

    feat: 项目案例的配置

--- Activiti/src/main/java/org/tang/activiti/App.java
+++ Activiti/src/main/java/org/tang/activiti/App.java
@@ -1,5 +1,6 @@
 package org.tang.activiti;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  <p> Activiti的启动文件 </p>
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class App 
 {
     public static void main( String[] args )
