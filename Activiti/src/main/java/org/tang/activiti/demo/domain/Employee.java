dff05c407e785a0e77d079cca3a3c2e38388a4c5
tangxing <17611260510@163.com>
2019-11-19 23:11:19 +0800

    feat: 实体对象类

---
+++ Activiti/src/main/java/org/tang/activiti/demo/domain/Employee.java
@@ -0,0 +1,45 @@
+package org.tang.activiti.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 
+ * @date   2019年11月19日
+ * @author tangxing
+ * @desc   <p> 雇员 </p>
+ */
+@Table(name = "t_employee")
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class Employee extends AbstractPersistable<Integer>{
+
+	@NotNull(message = "雇员名称不能为空")
+	private String name;
+	
+	@NotNull(message = "密码不能为空")
+	@Length(min = 6,message = "密码长度最少6位")
+	private String password;
+	
+	@Email(message = "邮箱不符合规则")
+	private String email;
+	
+	private String role;
+	
+	// 多对一  使用JoinColumn来指定外键   外键存储在多的一方
+	@ManyToOne(targetEntity = Employee.class)
+	@JoinColumn(name = "manager_id")
+	private Employee manager;
+	
+}
---
+++ Activiti/src/main/java/org/tang/activiti/demo/domain/LeaveBill.java
@@ -0,0 +1,49 @@
+package org.tang.activiti.demo.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Table(name = "t_leavebill")
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class LeaveBill extends AbstractPersistable<Integer> {
+
+	@NotNull(message = "请假天数不能为空")
+	private Integer days;
+	
+	@NotNull(message = "请假内容不能为空")
+	private String content;
+	
+	@Column(name = "leave_date")
+	private Date leaveDate = new Date();
+	
+	@ManyToOne
+	@JoinColumn(name = "employee_id")
+	@NotNull(message = "雇员不能为空")
+	private Employee employee;
+	
+	// 备注
+	private String remark;
+	
+	// 请假状态
+	private Integer state;
+	
+	// 录入
+	public static final Integer STATE_NEW = 0;
+	// 审批
+	public static final Integer STATE_AUDIT = 1;
+	// 审批完成
+	public static final Integer STATE_COMPLETE = 2;
+}
