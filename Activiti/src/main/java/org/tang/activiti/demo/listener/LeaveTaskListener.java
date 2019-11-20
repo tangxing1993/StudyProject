ea26c22e143ccad2006edb4061f7e1a0f1b29b20
tangxing <17611260510@163.com>
2019-11-20 23:58:23 +0800

    feat: 请假单的任务监听器

---
+++ Activiti/src/main/java/org/tang/activiti/demo/listener/LeaveTaskListener.java
@@ -0,0 +1,24 @@
+package org.tang.activiti.demo.listener;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+/**
+ * 
+ * @date   2019年11月20日
+ * @author tangxing
+ * @desc   <p> 请假单的任务监听器 </p>
+ */
+public class LeaveTaskListener implements TaskListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void notify(DelegateTask delegateTask) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
